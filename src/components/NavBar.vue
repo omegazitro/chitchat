3a16d06de7b71b40be126ef80b8e9d742247fea9
omegazitro <morti137@stu.southtexascollege.edu>
2021-04-22 22:32:08 -0500

    Added Navbar and Auth

---
+++ src/components/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="bg-blue-900 text-coolGray-300 py-6">
+    <div class="container mx-auto flex justify-between items-center">
+      <div class="brand flex space-x-2 text-3xl">
+        <mdi:message-processing-outline class="text-orange-300" />
+        <h1 class="font-thin">
+          Chit<span class="font-bold text-orange-300">Chat</span>
+        </h1>
+      </div>
+      <div class="menu">
+        <div v-if="!isAuthenticated">
+          <button
+            class="bg-green-700 text-white py-2 px-4 rounded-full hover:bg-green-900 focus:outline-non focus:ring-4 focus:ring-green-500"
+            @click="googlePopup"
+          >
+            Login
+          </button>
+        </div>
+        <div v-else class="flex items-center space-x-8">
+          <p>Welcome {{ user?.displayName }}</p>
+          <button
+            class="bg-red-700 text-white py-2 px-4 rounded-full hover:bg-red-900 focus:outline-non focus:ring-4 focus:ring-green-500"
+            @click="signOut"
+          >
+            Logout
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { authentication } from '~/helpers/useFirebase'
+const { isAuthenticated, user, googlePopup, signOut } = authentication()
+</script>
---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCPZqblxzcllrEZ3GvB_w57U8zHck5eVoc",
+    authDomain: "chitchat-95f37.firebaseapp.com",
+    projectId: "chitchat-95f37",
+    storageBucket: "chitchat-95f37.appspot.com",
+    messagingSenderId: "1099352888620",
+    appId: "1:1099352888620:web:3c4d275fe9dc10908c8889"
+  };
